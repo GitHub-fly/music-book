e3ccf4aab2e29468e1219324eff68c18160ca820
flygit <1224178565@qq.com>
2020-11-12 13:22:01 +0800

    初始化项目、音乐播放器的调用

---
+++ shop-book/pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class=""><myaudio></myaudio></view>
+</template>
+
+<script>
+import myaudio from '../../components/myaudio.vue'
+
+export default {
+	data() {
+		return {
+		}
+	},
+	components: {
+		myaudio
+	},
+	onLoad() {
+
+	},
+	methods: {
+
+	}
+}
+</script>
+
+<style></style>
---
+++ shop-book/store/audio/musicResources.js
@@ -0,0 +1,103 @@
+export default {
+	musicResources: [
+		{
+			id: 20001,
+			name: '女孩你为何踮脚尖',
+			src: '/static/mp3/20001.mp3',
+			singer: {
+				name: '黄晨晨',
+				synopsis: '黄晨晨 - 女孩你为何踮脚尖'
+			}
+		},
+		{
+			id: 20002,
+			name: '夏天的风',
+			src: '/static/mp3/20002.mp3',
+			singer: {
+				name: '火羊瞌睡了',
+				synopsis: '火羊瞌睡了 - 夏天的风'
+			}
+		},
+		{
+			id: 20003,
+			name: '我们的爱',
+			src: '/static/mp3/20003.mp3',
+			singer: {
+				name: '简弘亦',
+				synopsis: '简弘亦 - 我们的爱'
+			}
+		},
+		{
+			id: 20004,
+			name: '忘了',
+			src: '/static/mp3/20004.mp3',
+			singer: {
+				name: '江皓南',
+				synopsis: '江皓南 - 忘了'
+			}
+		},
+		{
+			id: 20005,
+			name: '情愿',
+			src: '/static/mp3/20005.mp3',
+			singer: {
+				name: '刘莉旻',
+				synopsis: '刘莉旻 - 情愿'
+			}
+		},
+		{
+			id: 20006,
+			name: '来不及（Live）',
+			src: '/static/mp3/20006.mp3',
+			singer: {
+				name: '刘瑞琦',
+				synopsis: '刘瑞琦 - 来不及（Live）'
+			}
+		},
+		{
+			id: 20007,
+			name: '一生啊',
+			src: '/static/mp3/20007.mp3',
+			singer: {
+				name: '马句',
+				synopsis: '马句 - 一生啊'
+			}
+		},
+		{
+			id: 20008,
+			name: '花花公子 (feat. step.jad)',
+			src: '/static/mp3/20008.mp3',
+			singer: {
+				name: '马思唯',
+				synopsis: '马思唯 - 花花公子 (feat. step.jad)'
+			}
+		},
+		{
+			id: 20009,
+			name: '想想念念',
+			src: '/static/mp3/20009.mp3',
+			singer: {
+				name: '皮卡丘多多',
+				synopsis: '皮卡丘多多 - 想想念念'
+			}
+		},
+		{
+			id: 20010,
+			name: '我爱他',
+			src: '/static/mp3/20010.mp3',
+			singer: {
+				name: '王小帅',
+				synopsis: '我爱他-王小帅'
+			}
+		},
+		{
+			id: 20011,
+			name: '最初的梦想',
+			src: '/static/mp3/20011.mp3',
+			singer: {
+				name: '徐婉婷',
+				synopsis: '最初的梦想'
+			}
+		},
+	]
+};
