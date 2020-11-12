e9d0828c996c2da8b6f396def317af7469db9d1a
flygit <1224178565@qq.com>
2020-11-12 15:52:12 +0800

    歌曲详情页面开发

--- shop-book/components/myaudio.vue
+++ shop-book/components/myaudio.vue
@@ -1,24 +1,29 @@
 <template>
-	<view class="fixed-bottom rounded mx-2 mb-1" style="height: 160rpx;background-color: #d1ccc0;opacity: .9;">
+	<view
+		class="fixed-bottom rounded mx-2 mb-1"
+		style="height: 160rpx;background-color: #d1ccc0;opacity: .9;"
+		@tap="toDetailPage"
+	>
 		<!-- 进度部分 -->
-		<view class="flex align-center justify-center font" style="color: #7a8388;height: 65rpx;">
-			<!-- 播放时刻 -->
-			<view>{{ currentTime | formatTime }}</view>
+		<view class="flex align-center justify-center font" style="color: #7a8388;height: 65rpx;" @tap.stop>
+			<!-- 总时长 -->
+			<view>{{ durationTime | formatTime }}</view>
 			<!-- 进度条部分 -->
 			<view style="width: 500rpx;">
 				<slider
-					@change="sliderToPlay"
-					@changing="sliderToPlay"
 					block-size="16"
 					activeColor="#e48267"
 					backgroundColor="#eef2f3"
 					:max="durationTime"
 					:value="currentTime"
+					@change="sliderToPlay"
+					@changing="sliderToPlay"
 				/>
 			</view>
-			<!-- 总时长 -->
-			<view>{{ durationTime | formatTime }}</view>
+			<!-- 播放时刻 -->
+			<view>{{ currentTime | formatTime }}</view>
 		</view>
+
 		<view class="flex align-center justify-between mx-2" style="height: 95rpx;">
 			<!-- 音频相关信息 -->
 			<view class="font" style="color: #424651;">
@@ -26,7 +31,7 @@
 				<view>歌曲-{{ audioName }}</view>
 			</view>
 			<!-- 音频按钮部分 -->
-			<view class="flex align-center">
+			<view class="flex align-center" @tap.stop>
 				<view class="animated" hover-class="pulse">
 					<my-icon iconId="icon-shangyishou" iconSize="75" @my-click="PreOrNext('pre')"></my-icon>
 				</view>
@@ -45,8 +50,6 @@
 </template>
 
 <script>
-
-// 辅助函数
 import { mapState, mapGetters, mapMutations, mapActions } from 'vuex';
 import unit from '../common/unit.js';
 export default {
@@ -63,20 +66,20 @@ export default {
 	},
 	methods: {
 		...mapMutations(['destruction']),
-		...mapActions(['init', 'PlayOrPause', 'PreOrNext', 'sliderToPlay'])
-	},
-	data() {
-		return {};
+		...mapActions(['init', 'PlayOrPause', 'PreOrNext', 'sliderToPlay']),
+		toDetailPage() {
+			uni.navigateTo({
+				url: '/pages/detail/detail'
+			});
+		}
 	},
 	mounted() {
 		this.init();
 	},
 	destroyed() {
-		ths.destruction();
-	},
-	onLoad() {
-		this.audio;
+		this.destruction();
 	}
 };
 </script>
+
 <style></style>
