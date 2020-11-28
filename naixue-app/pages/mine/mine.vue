427b73dc9435209c84db256ae5bc7a97987c2cc7
flygit <1224178565@qq.com>
2020-11-28 20:17:30 +0800

    登录功能

--- naixue-app/pages/mine/mine.vue
+++ naixue-app/pages/mine/mine.vue
@@ -1,22 +1,30 @@
 <template>
 	<view>
-		
+		<view v-if="isLogin" class="font-size-lg">
+			<text>{{ userInfo.nickName }}</text>
+		</view>
+		<view v-else class="font-size-lg font-weight-bold" @click="login">请点击授权登录</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
+import { mapState } from 'vuex';
+export default {
+	data() {
+		return {};
+	},
+	methods: {
+		// 登录
+		login() {
+			uni.navigateTo({
+				url: '/pages/login/login'
+			})
 		}
+	},
+	computed: {
+		...mapState(['userInfo', 'isLogin'])
 	}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
